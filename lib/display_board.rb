c53c8a752cc888c49233e1ede2f409125d96ea43
John Zimmerman <john.zimmerman@simplybusiness.com>
2019-03-02 17:17:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,11 +3,11 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def tix_tac_toe(board)
-puts " #{board(0)} | #{board(1)} | #{board(2)} "
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
-puts " #{board(3)} | #{board(4)} | #{board(5)} "
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
-puts " #{board(6)} | #{board(7)} | #{board(8)} "
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 tix_tac_toe(board)
